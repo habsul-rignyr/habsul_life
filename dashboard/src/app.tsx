e529f5e5852fcb2b90e978a79a6f050a1b0053a1
habsul <joshamy@gmail.com>
2022-03-26 20:24:23 +0800

    add navbar

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -1,20 +1,11 @@
-import { Logo } from './logo'
+import { Logo } from "./logo";
+import { Header, NavBar } from "./components/componentsIndex";
 
 export function App() {
   return (
     <>
-      <Logo />
-      <p>Hello Vite + Preact!</p>
-      <p>
-        <a
-          class="link"
-          href="https://preactjs.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Preact
-        </a>
-      </p>
+      <NavBar />
+      <Header />
     </>
-  )
+  );
 }
---
+++ dashboard/src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+            <h1 class = "roboto">Hello donkeys</h1>
+
+
+    )
+}
+
+export default Header;
